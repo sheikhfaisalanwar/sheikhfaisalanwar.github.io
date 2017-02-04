3bab4c97e8d241a9861f07c735e8db1863f31033
sheikhfaisalanwar <sheikhfaisalanwar@cmail.carleton.ca>
2017-02-04 08:05:24 -0500

    Initial sp logic

---
+++ 5brokeguyssplitter.py
@@ -0,0 +1,19 @@
+
+def splitty(boolean faisal,boolean pk, boolean akhond, boolean kashif, boolean rafin):
+
+    int faisal_paid =0
+    int pk_paid = 0
+    int akhond_paid = 0
+    int kashif_paid = 0
+    int rafin_paid = 0
+
+    if faisal:
+         faisal_paid =  raw_input('Enter the amount Faisal paid for this month-:\n')
+    elif pk
+         pk_paid = raw_input('Enter the amount pk paid for this month-:\n')
+    elif akhond
+         akhond_paid = raw_input('Enter the amount akhond paid for this month-:\n')
+    elif kashif
+         kashif_paid = raw_input('Enter the amount kashif paid for this month-:\n')
+    elif rafin
+         rafin_paid  =  raw_input('Enter the amount kashif paid for this month-:\n')
